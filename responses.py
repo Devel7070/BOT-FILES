d9afa26785e240a0d332588fb414b8741125f6b1
Devel <devel.2702@gmail.com>
2023-07-13 14:05:29 +0530

    bot_info token update

---
+++ responses.py
@@ -0,0 +1,9 @@
+import discord
+import random
+
+def get_response(message: str) -> str:
+    processed_message = message.lower()
+    if processed_message == 'Nigga' or 'nigga':
+        print('Dont say that word bruh')
+        
+
