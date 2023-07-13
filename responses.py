9a9306a594fa10838aefac5d2baa94dc0ced023f
Devel <devel.2702@gmail.com>
2023-07-13 14:12:41 +0530

    response edit + helpmessage

--- responses.py
+++ responses.py
@@ -5,5 +5,9 @@ def get_response(message: str) -> str:
     processed_message = message.lower()
     if processed_message == 'Nigga' or 'nigga':
         print('Dont say that word bruh')
-        
+    if processed_message == 'roll' or 'dice throw':
+        print(str(random.randint(1, 6)))
+    if processed_message == '!help':
+        print(help_message)
+
 
